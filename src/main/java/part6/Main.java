b345d6b97f02c84342b6696ed0cfc12560e9f09a
tyska1 <tina2skce@onet.pl>
2022-07-18 10:32:21 +0200

    part5

---
+++ src/main/java/part6/Main.java
@@ -0,0 +1,23 @@
+package part6;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("test.fxml"));
+                //Load(getClass().getResource("testCode.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
