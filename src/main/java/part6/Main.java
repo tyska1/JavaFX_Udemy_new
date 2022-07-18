331edeac05cf7300326daf48b7328431b8a49c12
tyska1 <tina2skce@onet.pl>
2022-07-18 13:23:27 +0200

    the end of second module

--- src/main/java/part6/Main.java
+++ src/main/java/part6/Main.java
@@ -1,17 +1,15 @@
 package part6;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.util.Random;
-
 public class Main extends Application {
 
     public static void main(String[] args) {
+
         launch(args);
     }
 
@@ -19,6 +17,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("testcode.fxml"));
         Scene scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
         primaryStage.setScene(scene);
         primaryStage.show();
     }
