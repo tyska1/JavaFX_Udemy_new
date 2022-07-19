c631d7aa7b573354bf2315fdb2875bb3b6a757e1
tyska1 <tina2skce@onet.pl>
2022-07-19 11:26:13 +0200

    module 7 episode 27 - progress features

--- src/main/java/javafx_properties/PropertiesController.java
+++ src/main/java/javafx_properties/PropertiesController.java
@@ -6,6 +6,8 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.ProgressIndicator;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -17,19 +19,26 @@ public class PropertiesController implements Initializable {
     @FXML
     private Label myLabel;
 
+    @FXML
+    private ProgressBar pb;
+
+    @FXML
+    private ProgressIndicator pi;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         ourPro.setProperty(0);
-        ourPro.propertyProperty().addListener(new ChangeListener<Object>() {
-            @Override
-            public void changed(ObservableValue<?> observableValue, Object oldValue, Object newValue) {
-                myLabel.setText(new Double(ourPro.getProperty()).toString());
-            }
-
-        });
+        ourPro.propertyProperty().addListener((ChangeListener<Object>) (observableValue, oldValue, newValue) -> myLabel.setText(new Double(ourPro.getProperty()).toString()));
+        pb.progressProperty().bind(ourPro.propertyProperty());
+        pi.progressProperty().bind(ourPro.propertyProperty());
     }
 
+
     public void buttonCLick(ActionEvent event){
-        ourPro.setProperty(ourPro.getProperty() - 1);
+        ourPro.setProperty(ourPro.getProperty() + 0.1);
+    }
+
+    public void buttonOneCLick(ActionEvent event){
+        ourPro.setProperty(ourPro.getProperty() - 0.1);
     }
 }
