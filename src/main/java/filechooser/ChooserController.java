757d7cf5ea7fbdfd2168bac3fbe77852ef871b59
tyska1 <tina2skce@onet.pl>
2022-07-19 10:13:51 +0200

    module 6 episode 25 - selecting multiple files

--- src/main/java/filechooser/ChooserController.java
+++ src/main/java/filechooser/ChooserController.java
@@ -6,6 +6,7 @@ import javafx.scene.control.*;
 import javafx.stage.FileChooser;
 
 import java.io.File;
+import java.util.List;
 
 public class ChooserController {
     @FXML
@@ -19,9 +20,12 @@ public class ChooserController {
         fc.setInitialDirectory(new File("C:\\Users"));
         //adding filter np. png files only *.png, pamiętać, że to stringi
         fc.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("PDF Files only", "*.pdf"));
-        File selectedFile = fc.showOpenDialog(null);
-        if(selectedFile != null){
-            listview.getItems().add(selectedFile.getName());
+        //File selectedFile = fc.showOpenDialog(null); <- selekcja pojedynczego pliku
+        List<File> selectedFiles = fc.showOpenMultipleDialog(null);
+        if(selectedFiles != null){
+            for(int x=0; x<selectedFiles.size(); x++){
+                listview.getItems().add(selectedFiles.get(x).getAbsolutePath());
+            }
         }
         else{
             System.out.println("File does not exist");
